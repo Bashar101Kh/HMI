4308bbbe76ed024e9402d45c572256d65dbed8e9
Bashar Alkhatib <57748024+Bashar101Kh@users.noreply.github.com>
2021-01-11 22:32:45 +0100

    changed the start method and partially implemented -user comand to extract cmd, param1(user), und param(message).

--- src/hmi/Console_Chat.java
+++ src/hmi/Console_Chat.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Console_Chat {
     //Fields
-    boolean run ;
-
-
+    private final String[] cmds = { "-help",
+                                    "-user",
+                                    "-m",
+                                    "-exit" };
+    private boolean run ;
 
     //Constructor
 
@@ -14,23 +16,44 @@ public class Console_Chat {
     //Methods
     void start(){
         run = true;
-        String input ;
+        String input,argUser;
+        String argMsg = "";
+        int len = 0 ;
         System.out.println("Welcome Bla Bla .. ");
 
         Scanner scanner = new Scanner(System.in);
         while (run){
             input = scanner.nextLine();
-            switch (input){
-                case "-help":   //list all commands
-                    System.out.println("no list jet");
-                    break;
-                case "-m":      //send Message
-                    System.out.println("-m not implemented jet");
-                    break;
-                case "-user":   //select user
-                    System.out.println("-user not implemented jet");
-                    break;
+            if (!input.isEmpty()){
+                len = input.length();
             }
+                // boolean a ? [if true]statment1 : [if false]statment 2 ;
+                // -m [userName] [Message]
+                // -user -[userName]
+                if(len >= cmds[0].length() ? input.substring(0,cmds[0].length()).equals(cmds[0]) : false){
+                    System.out.println("not implemented yet");
+                }else if (len >= cmds[1].length() ? input.substring(0,cmds[1].length()).equals(cmds[1]) : false){
+                    System.out.println("-user erkannt");
+                    String[] cmd_chunks = input.split(" ");
+                    argUser = cmd_chunks [1];
+                    for (int i = 2 ; i < cmd_chunks.length ; i++){
+                        argMsg += (i != cmd_chunks.length-1) ? (cmd_chunks[i]+" ") : (cmd_chunks[i]) ;
+                    }
+
+                    //send /view history
+                    System.out.println("Message:"+argMsg);
+                    System.out.println("User:"+argUser);
+                    //nach dem senden löschen
+                    argUser ="";
+                    argMsg = "";
+                }else if (len >= cmds[2].length() ? input.substring(0,cmds[2].length()).equals(cmds[2]): false){
+                    System.out.println("-user not implemented jet");
+                }else if (len >= cmds[3].length() ? input.substring(0,cmds[3].length()).equals(cmds[3]): false){
+                    run = false;
+                }else {
+                    System.out.println("Default");
+                }
+                input = null;
         }
     }
 }
