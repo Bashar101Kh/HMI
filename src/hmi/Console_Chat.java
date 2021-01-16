981dbb2ae05efb51e849456c780161dddcf1f862
Bashar <bashar-kh1@hotmail.com>
2021-01-16 18:43:23 +0100

    updated the cmds and the Console_Chat

--- src/hmi/Console_Chat.java
+++ src/hmi/Console_Chat.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Console_Chat {
-
+    enum cmds {
+        help,
+        exit,
+        newthread,
+        clear,
+        thread,
+        userlist,
+    }
     //Fields
-    private final String[] cmds = { "-help",
-                                    "-user",
-                                    "-m",
-                                    "-clear",
-                                    "-exit" };
     private boolean run ;
 
     //jo851hil TODO
@@ -34,8 +36,9 @@ public class Console_Chat {
     //Methods
     void start(){
         run = true;
-        String input,argUser;
-        String argMsg = "";
+        String input, argThreadTopic, argMsg;
+        argThreadTopic = "";
+        argMsg = "";
         int len = 0 ;
         System.out.println("Welcome Bla Bla .. ");
 
@@ -44,49 +47,43 @@ public class Console_Chat {
             input = scanner.nextLine();
             if (!input.isEmpty()){
                 len = input.length();
+            }else{
+                System.out.println("empty input !");
+
             }
                 // boolean a ? [if true]statment1 : [if false]statment 2 ;
                 // -m [userName] [Message]
                 // -user -[userName]
-                if(len >= cmds[0].length() ? input.substring(0,cmds[0].length()).equals(cmds[0]) : false){
+                if(input.equals("-"+ cmds.help.toString())){
                     System.out.println(cmdHelp);
-                }else if (len >= cmds[1].length() ? input.substring(0,cmds[1].length()).equals(cmds[1]) : false){
-                    System.out.println("-user erkannt");
+
+                }else if (input.equals("-"+ cmds.thread.toString())){
+                    System.out.println("-Thread command erkannt");
                     String[] cmd_chunks = input.split(" ");
-                    argUser = cmd_chunks.length>1? cmd_chunks [1]:"-list users not implemented jet !";
-                    if (cmd_chunks.length>2){
-                        for (int i = 2 ; i < cmd_chunks.length ; i++){
-                            argMsg += (i != cmd_chunks.length-1) ? (cmd_chunks[i]+" ") : (cmd_chunks[i]) ;
+                    if (input.length() == 1){                                   // extract the cmd
+                        System.out.println("you need to provide arguments");
+                        break;
+                    }
+                    if (input.length() == 2){                                   // extract arg1
+                        argThreadTopic = cmd_chunks[1];
+                        System.out.println("argThreadTopic: "+argThreadTopic);
+                        //TODO concret implemntation
+                    }
+                    if (cmd_chunks.length>2) {                                   // combine the rest as arg2
+                        for (int i = 2; i < cmd_chunks.length; i++) {
+                            argMsg += (i != cmd_chunks.length - 1) ? (cmd_chunks[i] + " ") : (cmd_chunks[i]);
                         }
-                    }else{
-                        System.out.print("view "+argUser+"'s chat history");
+                        //TODO concret implemntation
+
+                        argThreadTopic = "";
+                        argMsg = "";
                     }
 
-
-                    //jo851hil TODO tbd wie ReceiverID übernommen wird, diese müsste zuerst vom Storage bezogen werden
-                    if(argMsg != "") {
-                        sendThreadMessage = new ThreadMessage(argUser,argMsg);
-                        sendThreadMessage.createJSONFromMessage(); //Message class mit header (JSON) und data (byte[])
-                        sendThreadMessage.print();
-                    //jo851hil TODO umwandeln von header und data in in byte[] und übergabe an comMessage constructor, zusätzlich comMessage header generieren
-
-                        HMI_Directive HMIDirective = new HMI_Directive(sendThreadMessage.messageToByteArray(sendThreadMessage));
-                        receiveThreadMessage = new ThreadMessage();
-                        receiveThreadMessage.genThrMesFromHmiDir(HMIDirective);
-                        receiveThreadMessage.print();
-                    }
-
-                    //send /view history
-                    /*System.out.println("Message:"+argMsg);
-                    System.out.println("User:"+argUser);*/
-                    //nach dem senden löschen
-                    argUser ="";
-                    argMsg = "";
-                }else if (len >= cmds[2].length() ? input.substring(0,cmds[2].length()).equals(cmds[2]): false){
-                    System.out.println("-user not implemented jet");
-                }else if (len >= cmds[3].length() ? input.substring(0,cmds[3].length()).equals(cmds[3]): false){
+                }else if (input.equals("-"+ cmds.clear.toString())){
                     clearScreen();
-                }else if (len >= cmds[4].length() ? input.substring(0,cmds[4].length()).equals(cmds[4]): false){
+
+                }else if (input.equals("-"+ cmds.exit.toString())){
+                    System.out.println("quitting..");
                     run = false;
                 }else {
                     System.out.println("Default");
@@ -95,7 +92,6 @@ public class Console_Chat {
         }
     }
 
-    //TODO
     public static void clearScreen(){
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
